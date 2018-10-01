fba45cc0428878f372f96f355da5e61e84801bb7
telton <telton007@gmail.com>
2018-10-01 11:17:01 -0400

    Set up VueJS frontend.

---
+++ public/precache-manifest.8942bb4253fd774d86908d0013aa45e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a76557c1134c0ecb309c",
+    "url": "/js/chunk-vendors.408a0b51.js"
+  },
+  {
+    "revision": "aeca3ac7cd451e192209",
+    "url": "/js/app.218d7e0c.js"
+  },
+  {
+    "revision": "5ec49e6dccb2ca4932e9",
+    "url": "/js/about.07b90307.js"
+  },
+  {
+    "revision": "6e9abcf35d9b01d550569fde03e82c29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "aeca3ac7cd451e192209",
+    "url": "/css/app.9da4fc59.css"
+  }
+];
