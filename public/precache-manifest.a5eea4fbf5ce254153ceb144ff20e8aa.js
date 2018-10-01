c40dc176b403ea4ec0ccaaf792196b46a41a803c
telton <telton007@gmail.com>
2018-10-01 11:31:36 -0400

    Remove welcome view.

---
+++ public/precache-manifest.a5eea4fbf5ce254153ceb144ff20e8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a76557c1134c0ecb309c",
+    "url": "/js/chunk-vendors.408a0b51.js"
+  },
+  {
+    "revision": "271e3e0f1f7572cabac2",
+    "url": "/js/app.1e9e399c.js"
+  },
+  {
+    "revision": "5ec49e6dccb2ca4932e9",
+    "url": "/js/about.07b90307.js"
+  },
+  {
+    "revision": "3b39a370866505d423c34e829b873430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "271e3e0f1f7572cabac2",
+    "url": "/css/app.9da4fc59.css"
+  }
+];
