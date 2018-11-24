ffe0b05cf31aac71664a9e483241b5e7af08fb92
telton <telton007@gmail.com>
2018-11-24 16:31:14 -0500

    Add getter for user.

--- frontend/src/modules/firebase.js
+++ frontend/src/modules/firebase.js
@@ -1,49 +1,52 @@
-import FirebaseService from '@/services/FirebaseService';
-import { isEmpty } from 'lodash';
+import FirebaseService from "@/services/FirebaseService";
+import { isEmpty } from "lodash";
 
 export default {
-  namespaced: true,
-  state: {
-    user: {},
-  },
-  getters: {
-    isLoggedIn(state) {
-      let user = JSON.parse(localStorage.getItem('user'));
+    namespaced: true,
+    state: {
+        user: {},
+    },
+    getters: {
+        isLoggedIn(state) {
+            let user = JSON.parse(localStorage.getItem("user"));
 
-      if (user && !isEmpty(user)) {
-        state.user = user;
-      } else {
-        state.user = {};
-      }
+            return !isEmpty(state.user) && !isEmpty(user);
+        },
+        user() {
+            let user = JSON.parse(localStorage.getItem("user"));
 
-      return !isEmpty(state.user);
-    }
-  },
-  mutations: {
-    SET_USER(state) {
-      let user = FirebaseService.currentUser();
-      if (user != null) {
-        state.user = user;
-        localStorage.setItem('user', JSON.stringify(user));
-      } else {
-        state.user = {};
-        localStorage.setItem('user', JSON.stringify({}));
-      }
-    }
-  },
-  actions: {
-    CREATE_USER(store, { email, password }) {
-      return FirebaseService.createUser(email, password);
+            if (user && !isEmpty(user)) {
+                return user;
+            } else {
+                return null;
+            }
+        },
     },
-    LOG_IN(store, { email, password }) {
-      return FirebaseService.login(email, password);
+    mutations: {
+        SET_USER(state) {
+            let user = FirebaseService.currentUser();
+            if (user != null) {
+                state.user = user;
+                localStorage.setItem("user", JSON.stringify(user));
+            } else {
+                state.user = {};
+                localStorage.setItem("user", JSON.stringify({}));
+            }
+        },
     },
-    RESET_PASSWORD(store, { email }) {
-      return FirebaseService.resetPassword(email);
+    actions: {
+        CREATE_USER(store, { email, password }) {
+            return FirebaseService.createUser(email, password);
+        },
+        LOG_IN(store, { email, password }) {
+            return FirebaseService.login(email, password);
+        },
+        RESET_PASSWORD(store, { email }) {
+            return FirebaseService.resetPassword(email);
+        },
+        LOG_OUT() {
+            localStorage.removeItem("user");
+            return FirebaseService.logout();
+        },
     },
-    LOG_OUT() {
-      localStorage.removeItem('user');
-      return FirebaseService.logout();
-    }
-  }
 };
