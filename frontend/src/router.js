cd61c40ed3497310018ed269abcd34a87a46cc1c
telton <telton007@gmail.com>
2018-11-28 11:05:26 -0500

    auth/isLoggedIn returns true/false, not null.
    This prevented any auth checking, so an anonymous user could just visit any route. This also caused errors when an anonymous user attempted to upload a meme, because the Firebase uid was not available.

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,39 +1,39 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Login from './views/Login.vue';
-import Register from './views/Register.vue';
-import ResetPassword from './views/ResetPassword.vue';
-import store from './store';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+import ResetPassword from "./views/ResetPassword.vue";
+import store from "./store";
 
 Vue.use(Router);
 
 let router = new Router({
-    mode: 'history',
+    mode: "history",
     base: process.env.BASE_URL,
     routes: [
         {
-            path: '*',
-            redirect: '/login',
+            path: "*",
+            redirect: "/login",
         },
         {
-            path: '/login',
-            name: 'login',
+            path: "/login",
+            name: "login",
             component: Login,
         },
         {
-            path: '/register',
-            name: 'register',
+            path: "/register",
+            name: "register",
             component: Register,
         },
         {
-            path: '/reset-password',
-            name: 'reset-password',
-            component: ResetPassword
+            path: "/reset-password",
+            name: "reset-password",
+            component: ResetPassword,
         },
         {
-            path: '/',
-            name: 'home',
+            path: "/",
+            name: "home",
             component: Home,
             meta: {
                 requiresAuth: true,
@@ -44,18 +44,18 @@ let router = new Router({
 
 router.beforeEach((to, from, next) => {
     if (to.matched.some(record => record.meta.requiresAuth)) {
-      if (store.getters['auth/isLoggedIn'] == null) {
-        next({
-          path: '/login',
-          query: {
-            redirect: to.fullPath,
-          },
-        });
-      } else {
-        next();
-      }
+        if (!store.getters["auth/isLoggedIn"]) {
+            next({
+                path: "/login",
+                query: {
+                    redirect: to.fullPath,
+                },
+            });
+        } else {
+            next();
+        }
     } else {
-      next();
+        next();
     }
 });
 
